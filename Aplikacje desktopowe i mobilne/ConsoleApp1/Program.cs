5baf7768543f106881c49fbc81e3ce6efea20bcf
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-25 17:24:59 +0200

    dd

--- Aplikacje desktopowe i mobilne/ConsoleApp1/Program.cs
+++ Aplikacje desktopowe i mobilne/ConsoleApp1/Program.cs
@@ -1,7 +1,40 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
+﻿Console.WriteLine("Podaj  liczbe a");
+uint a = uint.Parse(Console.ReadLine());
+/*    if(a < 0)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Niepoprawna dana");
+        return;
     }
+    */
+
+Console.WriteLine("Podaj liczbe b");
+uint b = uint.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("NWD=" + a);
+
+/*
+ ***********************************************
+nazwa funkcji: NWD
+opis funkcji: najwiekszy wspolny dzielnik
+parametry: a - pierwsza liczba 
+           b- druga liczba
+zwracany typ i opis: uint- najwiekszy wspolny dzielnik
+autor: NR pesel 
+**********************************************
+*/
+uint NWD(uint a, uint b)
+{
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
 }
+
+
+
